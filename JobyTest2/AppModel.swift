49adb61581bd0414f922bd5d5bd33252291b3533
lpxdigital <40544717+lpxdigital@users.noreply.github.com>
2024-12-03 15:01:13 -0500

    Initial Commit

---
+++ JobyTest2/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  JobyTest2
+//
+//  Created by Jaime Lopez on 2024-12-03.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ JobyTest2/JobyTest2App.swift
@@ -0,0 +1,33 @@
+//
+//  JobyTest2App.swift
+//  JobyTest2
+//
+//  Created by Jaime Lopez on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct JobyTest2App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
